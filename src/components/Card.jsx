b4641701fd134fd7b991eb4dca2b618fdbf303ab
samadovoybek758 <oybek22samadov@gmail.com>
2024-10-04 17:27:01 +0500

    sd2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from 'react'
 
 function Card(props) {
     console.log(props);
-   
-    // return (proassName='container justify-center gap-4 mx-auto flex flex-row flex-wrap max-w-[1200px]'>
-    //       {data.length > 0 && data.map(function (product) {
-    //         return (
-    //           <div key={product.id} className='flex flex-col p-3 mx-auto w-[300px] border border-solid rounded-md border-gray-600'>
-    //             <h1>Name: {product.name}</h1>
-    //             <h2>Price: ${product.price}</h2>
-    //             <h2>Description: {product.description}</h2>
-    //           </div>
-    //         );
-    //       })}
-    //     </div>
+   const {name,price,description,id} = props.data
+    
+            return (
+              <div key={id} className='flex flex-col p-3 gap-2 mx-auto w-[300px] border border-solid rounded-md shadow-violet-400'>
+                <h1>Name: {name}</h1>
+                <h2>Price: ${price}</h2>
+                <h2>Description: {description}</h2>
+              </div>
+            );
+      
       
     }
 export default Card
